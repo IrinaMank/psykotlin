73ce0218c759a1b592a35694996b2ba1389e3c37
irina_mank <Irina.Mankovskaya@softomate.com>
2018-10-30 17:53:51 +0700

    Configure heroku again

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,21 +1,19 @@
-package blog
 
 import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-class App {
-    fun main(args: Array<String>) {
-        val host = "0.0.0.0"
-        val port = System.getenv("PORT").toInt()
-        embeddedServer(Netty, port) {
-            routing {
-                get("/") {
-                    call.respondText("My Example Blog", ContentType.Text.Html)
-                }
-            }
-        }.start(wait = true)
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            val text = "Howdy, Planet!"
+            call.respondText(text)
+        }
     }
 }
