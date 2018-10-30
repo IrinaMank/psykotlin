14dc51bb06bf71a91962e31050323265e5057e1d
irina_mank <Irina.Mankovskaya@softomate.com>
2018-10-30 18:14:08 +0700

    Configure db

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -2,12 +2,40 @@
 import io.ktor.application.*
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
-import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
+
+
+object GuestbookEntries: IntIdTable() {
+    val text = varchar("text", 255)
+    val creation = date("creation")
+}
+
+class GuestbookEntry(id: EntityID<Int>): IntEntity(id) {
+    companion object: IntEntityClass<GuestbookEntry>(GuestbookEntries)
+    var text by GuestbookEntries.text
+    var creation by GuestbookEntries.creation
+}
+
 fun Application.main() {
+    Database.connect(System.getenv("JDBC_DATABASE_URL"), driver = "org.postgresql.Driver")
+    transaction {
+        create(GuestbookEntries)
+        if(GuestbookEntry.count() == 0) {
+            GuestbookEntry.new {
+                text = "Thank you for stopping by!"
+                creation = DateTime.now()
+            }
+        }
+    }
     install(DefaultHeaders)
     install(CallLogging)
     install(Routing) {
