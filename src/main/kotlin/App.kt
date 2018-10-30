e415e25b0ae6a3176d9662d161e1ae6060743b79
irina_mank <Irina.Mankovskaya@softomate.com>
2018-10-30 16:33:17 +0700

    Configure heroku again

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -6,13 +6,14 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-
-fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("My Example Blog", ContentType.Text.Html)
+class App {
+    fun main(args: Array<String>) {
+        embeddedServer(Netty, 8080) {
+            routing {
+                get("/") {
+                    call.respondText("My Example Blog", ContentType.Text.Html)
+                }
             }
-        }
-    }.start(wait = true)
+        }.start(wait = true)
+    }
 }
